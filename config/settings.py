c73361e13c449e174f8847a2b63131c093c58427
seralex <alex.jungers@gmail.com>
2025-05-13 17:22:09 -0500

    Checkpoint 14: Switched from SQLite to PostgreSQL and successfully loaded cleaned seed data

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bga_league',
+        'USER': 'bgauser',
+        'PASSWORD': 'bgapassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
