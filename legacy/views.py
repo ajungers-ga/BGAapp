5157b625354c88fa6b61af5fd56d3cf0dac33539
seralex <alex.jungers@gmail.com>
2025-05-14 16:33:50 -0500

    Checkpoint 22: Added leaderboard status logic to Tour Legacy season view

--- legacy/views.py
+++ legacy/views.py
@@ -3,8 +3,10 @@ from results.models import Event
 
 # Create your views here.
 # Tour Legacy page (shows list of seasons)
+
+# telling DJANGO to give me a FLAT list of all unique seasons from the event table from top down
 def tour_legacy(request):
-    seasons = [2019, 2020, 2021, 2022, 2023, 2024]
+    seasons = Event.objects.values_list('season', flat=True).distinct().order_by('-season')
     return render(request, 'legacy/tour_legacy.html', {'seasons': seasons})
 
 def season_detail (request, season):
