b95351fced64e326d4cb14ae99e5822df6ff87e2
seralex <alex.jungers@gmail.com>
2025-05-19 10:41:42 -0500

    Study 5: Review and comment forms

--- players/forms.py
+++ players/forms.py
@@ -1,7 +1,24 @@
+# File Purpose: Form definitions for the PLAYERS app in the BGA Tour Tracker
+# This file defines a custom DJANGO form used to create or update PLAYER profiles.
+
+# - PlayerForm: allows users to enter or edit player data (name, nickname, hometown, HOF status, etc.)
+ 
+# This form pulls all fields from the Player model automatically.
+# It is actively used in views like /players/new/ and /players/<id>/edit/
+
+
+
+
+#---------IMPORT DEPENDENCIES----------#
 from django import forms
 from .models import Player
+#---------IMPORT DEPENDENCIES----------#
 
-class PlayerForm(forms.ModelForm):
+
+# ------------------------------Player Form-------------------------------#
+class PlayerForm(forms.ModelForm):  # Used in player create/edit views
     class Meta:
-        model = Player
-        fields = '__all__'
+        model = Player             # Ties this form to the Player model
+        fields = '__all__'         # Includes all fields (first_name, last_name, nickname, etc.)
+                                   # Form will render default widgets for each field
+# ------------------------------Player Form-------------------------------#
--- results/forms.py
+++ results/forms.py
@@ -1,33 +1,50 @@
-# File Purpose:
-# Defines the EventForm used to create and edit Event model entries.
+# File Purpose: Form definitions for the RESULTS app in the BGA Tour Tracker
+# This file defines custom DJANGO forms used to create or update EVENT and SCORE entries.
 # Adds a calendar widget and placeholder to the date field.
-# attrs is short for HTML attrbutes, in this case type and placeholder are used
+# - EventForm: for entering or editing tournaments
+# - ScoreForm: for entering or editing player scores
 
+# Widgets and attributes are used to control how fields appear in the browser.
+
+#---------IMPORT DEPENDENCIES----------#
 from django import forms
 from .models import Score, Event
-from players.models import Player
+# from players.models import Player # COMMENTED OUT BC-
+#(using player names in score form but treating player and teamate as plain charfields, not as model-bound fields)
 
-# ------- Score form ------- #
-class ScoreForm(forms.ModelForm):
+#---------IMPORT DEPENDENCIES----------#
+
+
+
+# ----------------------------------------Score form------------------------------------#
+class ScoreForm(forms.ModelForm): # Results/#/Leaderboard/addNewScore form
     player = forms.CharField(
         label='Player',
-        widget=forms.TextInput(attrs={'list': 'player-options', 'class': 'form-control'})
+        widget=forms.TextInput(attrs={'list': 'player-options', 'class': 'form-control'}) # This enables the autocomplete suggestions
     )
     teammate = forms.CharField(
         label='Teammate',
         required=False,
-        widget=forms.TextInput(attrs={'list': 'player-options', 'class': 'form-control'})
+        widget=forms.TextInput(attrs={'list': 'player-options', 'class': 'form-control'}) # Matches an HTML <datalist> element
     )
 
-    class Meta:
-        model = Score
-        fields = ['score']
+    # META is a INNER class in DJANGO used to store CONFIGURATION for things like: 
+    # forms, widgets, models & even serializers
+    class Meta:         
+        model = Score       # Telling DJANGO I want to use the SCORE model, 
+        fields = ['score']  # but ONLY include the SCORE field, (not all)
+                            # since the user MANUALLY CUSTOMIZES these fields, they are not included here
+# ----------------------------------------Score form------------------------------------#
 
-# ------- Event form ------- #
-class EventForm(forms.ModelForm):
-    class Meta:
+
+
+
+# -----------------------------Event form-----------------------------------------------#
+class EventForm(forms.ModelForm): # /tourResults/createNewEvent form
+    class Meta:     
         model = Event
         fields = '__all__'  # or ['name', 'date', 'course', 'format', etc.]
+        # BELOW = customize how the DATE field appears in the browser
         widgets = {
             'date': forms.DateInput(attrs={
                 'type': 'date',
@@ -35,3 +52,4 @@ class EventForm(forms.ModelForm):
                 'class': 'form-control'
             })
         }
+# -----------------------------Event form-----------------------------------------------#        
