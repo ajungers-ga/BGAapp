3d0a4efc078a323ec6dbcde6521ddbcb99ec7129
seralex <alex.jungers@gmail.com>
2025-05-26 14:53:37 -0500

    Checkpoint 31: Un-comment player_detail route to fix NoReverseMatch

--- players/urls.py
+++ players/urls.py
@@ -19,7 +19,7 @@ from .views import PlayerStatsView
 # #------------------------------DEFINE ROUTES------------------------------------------#
 urlpatterns = [
     path('', views.PlayerListView.as_view(), name='player_list'),
-#     path('<int:pk>/', views.PlayerDetailView.as_view(), name='player_detail'),
+    path('<int:pk>/', views.PlayerDetailView.as_view(), name='player_detail'),
 #     # path('<int:pk>/update/', views.PlayerUpdateView.as_view(), name='player_update'),
 #     path('<int:pk>/delete/', views.PlayerDeleteView.as_view(), name='player_delete'),
     path('stats/', PlayerStatsView.as_view(), name='player_stats'),
