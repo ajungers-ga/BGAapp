0ec693f92077c46dc3d915e187c659bff9dc8d2a
seralex <alex.jungers@gmail.com>
2025-05-26 15:09:46 -0500

    Checkpoint 6: Added smart back buttons to edit_score, event_form, and event_confirm_delete templates

--- players/urls.py
+++ players/urls.py
@@ -20,8 +20,6 @@ from .views import PlayerStatsView
 urlpatterns = [
     path('', views.PlayerListView.as_view(), name='player_list'),
     path('<int:pk>/', views.PlayerDetailView.as_view(), name='player_detail'),
-#     # path('<int:pk>/update/', views.PlayerUpdateView.as_view(), name='player_update'),
-#     path('<int:pk>/delete/', views.PlayerDeleteView.as_view(), name='player_delete'),
     path('stats/', PlayerStatsView.as_view(), name='player_stats'),
 ]
 #------------------------------DEFINE ROUTES------------------------------------------#
