7e45c54dea560d5c22a012fdd7f8ecc50ea29b5b
seralex <alex.jungers@gmail.com>
2025-05-29 22:31:40 -0500

    Pre Launch 22.22 – Fix: include 3rd and 4th players in dynamic event_wins on PlayerDetailView

--- players/views.py
+++ players/views.py
@@ -224,13 +224,11 @@ from django.db import models
 from .models import Player
 from results.models import Score
 
-
 #-------------------------------------------#
 class PlayerListView(ListView):
     model = Player
     template_name = 'bgaapp/player_list.html'
 #---------------------------------------------#
-
 
 #-----------------------------------------------------#
 class PlayerDetailView(DetailView):
@@ -241,12 +239,16 @@ class PlayerDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         player = self.get_object()
         context['event_wins'] = Score.objects.filter(
-            models.Q(player=player) | models.Q(teammate=player),
+            (
+                models.Q(player=player) |
+                models.Q(teammate=player) |
+                models.Q(third_player=player) |
+                models.Q(fourth_player=player)
+            ),
             placement__in=["1", "1st"]
         ).select_related("event")
         return context
 #------------------------------------------------------------#
-
 
 #------------------------------------------------------------#
 class PlayerStatsView(TemplateView):
