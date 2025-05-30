cb56d9414e3cb2b866dea1c028fc792529fd4b8b
seralex <alex.jungers@gmail.com>
2025-05-30 13:10:31 -0500

    Pre Launch 23.15: Simplify PlayerStatsView to use model properties

--- players/views.py
+++ players/views.py
@@ -1,7 +1,5 @@
 from django.views.generic import ListView, DetailView
 from django.db import models
-from django.db.models import Count, Q, F, FloatField, ExpressionWrapper
-from django.db.models.functions import Coalesce, NullIf
 from .models import Player
 from results.models import Score
 
@@ -47,20 +45,10 @@ class PlayerStatsView(ListView):
     paginate_by = 25
 
     def get_queryset(self):
-        players = Player.objects.all().prefetch_related(
+        return Player.objects.all().prefetch_related(
             'scores',
             'teammate_scores',
             'third_player_scores',
             'fourth_player_scores'
-        ).annotate(
-            scores_played=Count('scores'),
-            teammate_scores_played=Count('teammate_scores'),
-            third_player_scores_played=Count('third_player_scores'),
-            fourth_player_scores_played=Count('fourth_player_scores'),
-            scores_wins=Count('scores', filter=Q(scores__placement__in=["1", "1st"])),
-            teammate_scores_wins=Count('teammate_scores', filter=Q(teammate_scores__placement__in=["1", "1st"])),
-            third_player_scores_wins=Count('third_player_scores', filter=Q(third_player_scores__placement__in=["1", "1st"])),
-            fourth_player_scores_wins=Count('fourth_player_scores', filter=Q(fourth_player_scores__placement__in=["1", "1st"]))
-        )
-        return players
+        ).order_by('-id')
 #------------------------------------------------------------#
