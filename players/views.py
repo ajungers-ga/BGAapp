5efa5522be46609f653ed86fb136057c26039bf4
seralex <alex.jungers@gmail.com>
2025-05-30 14:06:22 -0500

    Pre Launch 23.17: Fix PlayerStatsView ordering and win percentage calculation

--- players/views.py
+++ players/views.py
@@ -1,5 +1,4 @@
 
-
 from django.views.generic import ListView, DetailView
 from django.db import models
 from .models import Player
@@ -47,10 +46,25 @@ class PlayerStatsView(ListView):
     paginate_by = 25
 
     def get_queryset(self):
-        return Player.objects.all().prefetch_related(
+        players = Player.objects.all().prefetch_related(
             'scores',
             'teammate_scores',
             'third_player_scores',
             'fourth_player_scores'
-        ).order_by('-id')
+        )
+        # Sort by events played descending
+        players = sorted(players, key=lambda p: p.career_events_played, reverse=True)
+        return players
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add win percentage to each player object
+        for player in context['object_list']:
+            events_played = player.career_events_played
+            wins = player.career_wins
+            if events_played > 0:
+                player.win_percentage = round((wins / events_played) * 100, 1)
+            else:
+                player.win_percentage = 0.0
+        return context
 #------------------------------------------------------------#
