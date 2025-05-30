2285874749a48e09f6a6b32bbc7b7e961632b3eb
seralex <alex.jungers@gmail.com>
2025-05-30 12:05:58 -0500

    Pre Launch 23.0: Optimized PlayerListView and PlayerStatsView for faster load times

--- players/views.py
+++ players/views.py
@@ -221,6 +221,7 @@
 
 from django.views.generic import ListView, DetailView, TemplateView
 from django.db import models
+from django.db.models import Count, Q
 from .models import Player
 from results.models import Score
 
@@ -228,6 +229,12 @@ from results.models import Score
 class PlayerListView(ListView):
     model = Player
     template_name = 'bgaapp/player_list.html'
+
+    # Optimize with select_related/prefetch_related if needed
+    def get_queryset(self):
+        return Player.objects.all().prefetch_related(
+            'score_set'  # Adjust based on actual related name
+        )
 #---------------------------------------------#
 
 #-----------------------------------------------------#
@@ -240,10 +247,10 @@ class PlayerDetailView(DetailView):
         player = self.get_object()
         context['event_wins'] = Score.objects.filter(
             (
-                models.Q(player=player) |
-                models.Q(teammate=player) |
-                models.Q(third_player=player) |
-                models.Q(fourth_player=player)
+                Q(player=player) |
+                Q(teammate=player) |
+                Q(third_player=player) |
+                Q(fourth_player=player)
             ),
             placement__in=["1", "1st"]
         ).select_related("event")
@@ -256,11 +263,19 @@ class PlayerStatsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        players = sorted(
-            Player.objects.all(),
-            key=lambda p: p.career_wins,
-            reverse=True
-        )
+
+        # Prefetch related scores to avoid N+1 queries
+        players = Player.objects.all().prefetch_related('score_set')
+
+        # If possible, aggregate career_wins in the database
+        players = players.annotate(
+            total_wins=Count(
+                'score__id',
+                filter=Q(score__placement__in=["1", "1st"])
+            )
+        ).order_by('-total_wins')
+
         context['players'] = players
         return context
 #------------------------------------------------------------#
+
