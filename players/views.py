47b303054f74e27522fde73c54476bd24d3bb62e
seralex <alex.jungers@gmail.com>
2025-05-30 12:39:35 -0500

    Pre Launch 23.6: Restored PlayerListView and fixed PlayerStatsView to prevent division by zero

--- players/views.py
+++ players/views.py
@@ -1,9 +1,44 @@
 from django.views.generic import ListView, DetailView
 from django.db import models
 from django.db.models import Count, Q, F, FloatField, ExpressionWrapper
-from django.db.models.functions import Coalesce
+from django.db.models.functions import Coalesce, NullIf
 from .models import Player
 from results.models import Score
+
+#-------------------------------------------#
+class PlayerListView(ListView):
+    model = Player
+    template_name = 'bgaapp/player_list.html'
+    paginate_by = 25
+
+    def get_queryset(self):
+        return Player.objects.all().prefetch_related(
+            'scores',
+            'teammate_scores',
+            'third_player_scores',
+            'fourth_player_scores'
+        )
+#---------------------------------------------#
+
+#-----------------------------------------------------#
+class PlayerDetailView(DetailView):
+    model = Player
+    template_name = 'bgaapp/player_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        player = self.get_object()
+        context['event_wins'] = Score.objects.filter(
+            (
+                models.Q(player=player) |
+                models.Q(teammate=player) |
+                models.Q(third_player=player) |
+                models.Q(fourth_player=player)
+            ),
+            placement__in=["1", "1st"]
+        ).select_related("event")
+        return context
+#------------------------------------------------------------#
 
 #------------------------------------------------------------#
 class PlayerStatsView(ListView):
@@ -18,13 +53,12 @@ class PlayerStatsView(ListView):
             'third_player_scores',
             'fourth_player_scores'
         ).annotate(
-            events_played=Count(
-                'scores__event',
-                distinct=True
-            ) + 
-            Count('teammate_scores__event', distinct=True) +
-            Count('third_player_scores__event', distinct=True) +
-            Count('fourth_player_scores__event', distinct=True),
+            events_played=(
+                Count('scores__event', distinct=True) +
+                Count('teammate_scores__event', distinct=True) +
+                Count('third_player_scores__event', distinct=True) +
+                Count('fourth_player_scores__event', distinct=True)
+            ),
             career_wins=(
                 Coalesce(Count('scores', filter=Q(scores__placement__in=["1", "1st"]), distinct=True), 0) +
                 Coalesce(Count('teammate_scores', filter=Q(teammate_scores__placement__in=["1", "1st"]), distinct=True), 0) +
@@ -33,7 +67,7 @@ class PlayerStatsView(ListView):
             )
         ).annotate(
             win_percentage=ExpressionWrapper(
-                F('career_wins') * 100.0 / F('events_played'),
+                F('career_wins') * 100.0 / Coalesce(NullIf(F('events_played'), 0), 1),
                 output_field=FloatField()
             )
         ).order_by('-events_played')
